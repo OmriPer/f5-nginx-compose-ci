b1ad1c60d53b49905e635799496bbb723369cfa8
Omri <omri.perry42@gmail.com>
2024-11-09 17:01:59 +0200

    implementation 1

---
+++ test/test.py
@@ -0,0 +1,35 @@
+# test.py
+
+import requests
+from typing import Optional
+
+def test_server(url: str, expected_status: int, expected_content: Optional[str]=None) -> bool: 
+    with requests.get(url) as response:
+        # check the response code
+        if response.status_code == expected_status:
+            print(f'server {url} responded with the correct status')
+        else:
+            print(f'server {url} responded with the wrong status')
+            return False
+
+        # check the response body if applicable
+        if expected_content is not None:
+            if response.text.strip() == expected_content.strip():
+                print(f'server {url} responded with the correct content')
+            else:
+                print(f'server {url} responded with the wrong content')
+                return False
+
+        return True
+    
+if __name__ == '__main__':
+    success: bool = True
+    with open('expected.html', 'r') as expected_file:
+        expected_html:str = expected_file.read()
+    
+    success = test_server('http://nginx:8080', 200, expected_html) and test_server('http://nginx:8081', 500)
+
+    result_file_name = '/result/succeeded' if success else '/result/fail'
+
+    with open(result_file_name, 'w') as result_file:
+        result_file.write('')
